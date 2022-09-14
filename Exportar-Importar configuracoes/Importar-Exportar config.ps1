230cbec2728934dc9d8561b1a2bf4d6333d3551d
IIgorAugustoI <76701857+IIgorAugustoI@users.noreply.github.com>
2022-09-14 11:38:47 -0300

    Arquivo para exportar ou importar configurações

---
+++ Exportar-Importar configuracoes/Importar-Exportar config.ps1
@@ -0,0 +1,91 @@
+﻿############### Script para Importação das Config's AtivaDaruma ###############################
+############### Verificação se o script está sendo executado como Administrador ###############
+
+param([switch]$Elevated)
+function Test-Admin {
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) 
+    {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition));   
+}
+exit
+}
+
+function ImportarAtivaDaruma($diretorio) {
+
+    Write-Output "@echo off 
+    start AtivaDaruma.exe" > $diretorio\AtivaDaruma.bat;
+    Copy-Item $diretorio\AtivaDaruma.bat 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\';
+
+    $hostname = 'HOSTNAME=' + (Get-Content env:computername) + '"';
+    $caminhoServidor = ((Get-Content -Path $diretorio\Backup_AtivaDaruma\Config_AtivaDaruma.reg) | Select-String '"Caminho_servidor"="') -replace '"Caminho_servidor"="','';
+
+    if ($caminhoServidor -match 'HOSNTAME=*' -or $caminhoServidor -match 'hostname=*'){
+        (Get-Content -Path $diretorio\Backup_AtivaDaruma\Config_AtivaDaruma.reg) -replace $caminhoServidor, $hostname | Set-Content -Path $diretorio\NovaConfig_AtivaDaruma.reg;
+            reg IMPORT $diretorio\NovaConfig_AtivaDaruma.reg;
+    } else{
+            reg IMPORT $diretorio\Backup_AtivaDaruma\Config_AtivaDaruma.reg;
+    }
+
+    Set-Location $diretorio;
+    Copy-Item $diretorio\Backup_AtivaDaruma\Local.ini $diretorio;
+
+    Invoke-WebRequest https://ip225.ip-149-56-155.net/owncloud/s/N8N9tXRy4GRrwXw/download -OutFile $diretorio\CHNetDLL_Instalacao.rar;
+    Invoke-WebRequest https://ip225.ip-149-56-155.net/owncloud/s/SMzu2IssIw26P1U/download -OutFile $diretorio\RegAsm.rar;
+
+    Clear-Host;
+    'Extraia a CHNetDLL_Instalacao e o RegAsm antes de continuar.'
+    Pause;
+
+    .\RegAsm.exe CHNetDLL.dll /tlb:chnetdll.tlb;
+    Clear-Host;
+
+    'Atualize a CHNETDLL e o AtivaDaruma';
+    Pause;
+    Exit;
+}
+function ExportarAtivaDaruma($diretorio) {
+    Remove-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\AtivaDaruma.*" -Recurse;
+    Set-Location $diretorio
+    mkdir .\Backup_AtivaDaruma;
+    reg export "HKCU\SOFTWARE\VB and VBA Program Settings\Chianca Softwares\Memória" $diretorio\Backup_AtivaDaruma\Config_AtivaDaruma.reg /y;
+    Copy-Item $diretorio\local.ini $diretorio\Backup_AtivaDaruma;
+    Remove-Item -Path $diretorio\AtivaDaruma.*;
+    Compress-Archive -Path $diretorio\Backup_AtivaDaruma -DestinationPath Backup_AtivaDaruma.zip;
+    Remove-Item $diretorio\Backup_AtivaDaruma -Recurse
+    'Backup Concluído';
+}
+
+'INFORME O CAMINHO DA INSTALAÇÃO DO ATIVADARUMA';
+
+[int]$caminho = Read-Host "# Digite 1 para Chianca # # Digite 2 para Chianca Softwares #";
+
+if($caminho -eq 1) {
+    $diretorio = 'C:\Chianca';
+} elseif ($caminho -eq 2){
+    $diretorio = 'C:\Program Files (x86)\Chianca Softwares';
+}elseif ($caminho -ne 1 -and 2) {
+    'Write-Host Selecione uma opção correta.'
+    exit;
+};
+
+$opcao = Read-Host '# Digite 1 para importar as configurações # # Digite 2 para exportar as configurações #';
+
+switch ($opcao) {
+    1 { 
+        ImportarAtivaDaruma($diretorio);
+     }
+     2 {
+        ExportarAtivaDaruma($diretorio);
+     }
+    Default {
+        'Selecione a opção correta.';
+    }
+}
+
+exit;
